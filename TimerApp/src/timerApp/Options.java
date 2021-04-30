961438afd74da17df8a848629f5dbf2314ef859b
Dejan Jovanovic <83391390+DejanJovanovic021@users.noreply.github.com>
2021-04-30 21:26:46 +0200

    Timer Swing Application

---
+++ TimerApp/src/timerApp/Options.java
@@ -0,0 +1,50 @@
+
+package timerApp;
+
+import java.awt.Color;
+
+
+public class Options {                                                          
+    
+    private String onTime;
+    private int countdown;
+    private Color color;
+    private int speed;
+
+    public String getOnTime() {
+        return onTime;
+    }
+
+    public void setOnTime(String onTime) {
+        this.onTime = onTime;
+    }
+
+    public int getCountdown() {
+        return countdown;
+    }
+
+    public void setCountdown(int countdown) {
+        this.countdown = countdown;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    
+
+    
+    
+}
